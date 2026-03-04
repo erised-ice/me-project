677a305e296512a039791584a27e2c6593c3606f
alexandra <le.onze.mai@gmail.com>
2026-03-04 21:57:48 +0100

    Исправлена перезагрузка страницы рецепта

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,12 +1,19 @@
 import {Layout} from "../shared/components/Layout/Layout.tsx";
 import {useParams} from "react-router-dom";
-import {useAppSelector} from "../shared/hooks/redux.tsx";
+import {useAppDispatch, useAppSelector} from "../shared/hooks/redux.tsx";
 import {selectRecipes} from "../features/recipes/selectors.ts";
+import {useEffect} from "react";
+import {fetchRecipes} from "../features/recipes/thunks.ts";
 
 export const RecipePage = () => {
+  const dispatch = useAppDispatch();
   const recipeId = useParams().id;
   const recipes = useAppSelector(selectRecipes);
   const recipe = recipes.find(item => item.id === Number(recipeId));
+
+  useEffect(() => {
+    dispatch(fetchRecipes());
+  }, [dispatch]);
 
   return (
     <Layout>
