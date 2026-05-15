23b5b29f518bd2738b14432976dbe808f9da6e87
alexandra <le.onze.mai@gmail.com>
2026-05-15 13:38:09 +0200

    Add mobile menu

--- src/pages/_shared/Layout/components/Header/Header.tsx
+++ src/pages/_shared/Layout/components/Header/Header.tsx
@@ -1,4 +1,5 @@
-import { AppShell, Container, Group } from '@mantine/core';
+import { AppShell, Burger, Container, Drawer, Group, Stack } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { useTranslation } from 'react-i18next';
 import { LanguageSwitcher } from '@/widgets/LanguageSwitcher/LanguageSwitcher.tsx';
 import { Button, Link } from '@/shared/components';
@@ -6,6 +7,7 @@ import { ROUTE } from '@/shared/constants/routes.ts';
 
 export const Header = () => {
   const { t } = useTranslation();
+  const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <AppShell.Header>
@@ -13,7 +15,7 @@ export const Header = () => {
       Здесь будет навигация, логотип и тому подобное. Он будет одинаковый на всех страницах
       */}
       <Container size="lg" px="md" h="100%">
-        <Group h="100%">
+        <Group h="100%" visibleFrom="sm">
           <Button>
             <Link to={ROUTE.HOME}>{t('navigation.home')}</Link>
           </Button>
@@ -22,7 +24,21 @@ export const Header = () => {
           </Button>
           <LanguageSwitcher />
         </Group>
+        <Group h="100%" hiddenFrom="sm">
+          <Burger opened={opened} onClick={open} color="cyan" />
+        </Group>
       </Container>
+      <Drawer opened={opened} onClose={close} hiddenFrom="sm">
+        <Stack>
+          <LanguageSwitcher />
+          <Button>
+            <Link to={ROUTE.HOME}>{t('navigation.home')}</Link>
+          </Button>
+          <Button>
+            <Link to={ROUTE.RECIPES}>{t('navigation.recipes')}</Link>
+          </Button>
+        </Stack>
+      </Drawer>
       {/*TODO: Сделать так, чтобы подсвечивались нужные ссылки когда находимся на нужной странице */}
     </AppShell.Header>
   );
